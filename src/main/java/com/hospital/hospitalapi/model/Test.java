a48ce43ce07e65758cfb3154091f0c7d56924ea8
Simion Jardan <simionjardan@Simions-MacBook-Pro.local>
2022-02-24 09:18:31 -0600

    created test model class

---
+++ src/main/java/com/hospital/hospitalapi/model/Test.java
@@ -0,0 +1,119 @@
+package com.hospital.hospitalapi.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tests")
+public class Test {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String testDate;
+
+    @Column
+    private String testTime;
+
+    @Column
+    private String testResult;
+
+    @ManyToOne
+    @JoinColumn(name = "patient_id")
+    @JsonIgnore
+    private Patient patient;
+
+    @ManyToOne
+    @JsonIgnore
+    @JoinColumn(name = "doctor_id")
+    private Doctor doctor;
+
+    public Test() {
+    }
+
+    public Test(Long id, String name, String testDate, String testTime, String testResult, Patient patient, Doctor doctor) {
+        this.id = id;
+        this.name = name;
+        this.testDate = testDate;
+        this.testTime = testTime;
+        this.testResult = testResult;
+        this.patient = patient;
+        this.doctor = doctor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTestDate() {
+        return testDate;
+    }
+
+    public void setTestDate(String testDate) {
+        this.testDate = testDate;
+    }
+
+    public String getTestTime() {
+        return testTime;
+    }
+
+    public void setTestTime(String testTime) {
+        this.testTime = testTime;
+    }
+
+    public String getTestResult() {
+        return testResult;
+    }
+
+    public void setTestResult(String testResult) {
+        this.testResult = testResult;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    @Override
+    public String toString() {
+        return "Test{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", testDate='" + testDate + '\'' +
+                ", testTime='" + testTime + '\'' +
+                ", testResult='" + testResult + '\'' +
+                ", patient=" + patient +
+                ", doctor=" + doctor +
+                '}';
+    }
+}

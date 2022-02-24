34e162c7d8cf31a8d673fd4c957ce201e75f3826
Simion Jardan <simionjardan@Simions-MacBook-Pro.local>
2022-02-24 09:01:47 -0600

    generated geteters and setters for doctor model class

--- src/main/java/com/hospital/hospitalapi/model/Doctor.java
+++ src/main/java/com/hospital/hospitalapi/model/Doctor.java
@@ -26,4 +26,65 @@ public class Doctor {
     @LazyCollection(LazyCollectionOption.FALSE)
     private List<Test> testList;
 
+    public Doctor() {
+    }
+
+    public Doctor(Long id, String doctorName, String specialization, List<Patient> patientList, List<Test> testList) {
+        this.id = id;
+        this.doctorName = doctorName;
+        this.specialization = specialization;
+        this.patientList = patientList;
+        this.testList = testList;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDoctorName() {
+        return doctorName;
+    }
+
+    public void setDoctorName(String doctorName) {
+        this.doctorName = doctorName;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public List<Patient> getPatientList() {
+        return patientList;
+    }
+
+    public void setPatientList(List<Patient> patientList) {
+        this.patientList = patientList;
+    }
+
+    public List<Test> getTestList() {
+        return testList;
+    }
+
+    public void setTestList(List<Test> testList) {
+        this.testList = testList;
+    }
+
+    @Override
+    public String toString() {
+        return "Doctor{" +
+                "id=" + id +
+                ", doctorName='" + doctorName + '\'' +
+                ", specialization='" + specialization + '\'' +
+                ", patientList=" + patientList +
+                ", testList=" + testList +
+                '}';
+    }
 }
